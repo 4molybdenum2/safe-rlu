05d97c9df3ae52b7ea070b1fdd49d510419afac4
Tathagata Paul <tathagatapaul7@gmail.com>
2024-05-04 00:54:32 -0400

    add btree set benchmark
    Signed-off-by: Tathagata Paul <tathagatapaul7@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,42 @@
 # Read Log Update in Rust
 
 Implementation of Read Log Update in Rust
+
+
+### Running Benchmarks:
+
+To run the single object benchmarks
+
+```bash
+cargo run --bin benchmark --release
+```
+
+To run RLU based Set benchmarks
+
+```bash
+cargo run --bin benchmark_rlu_set --release
+```
+
+
+To run Rust's BTree Set benchmarks
+
+```bash
+cargo run --bin benchmark_btree_set --release
+```
+
+
+![BTree Set Benchmark](bench_btree.png)
+
+### Benchmarking Plot
+
+To install necessary libraries for making the benchmark plots:
+
+```bash
+pip install -r requirements.txt
+```
+
+To run the benchmark plots:
+
+```bash
+python bench_plot.py
+```
---
+++ src/bin/benchmark_btree_set.rs
@@ -0,0 +1,135 @@
+#![allow(dead_code, unused_variables)]
+
+extern crate rand;
+
+use rlu::{ConcurrentBTreeSet, ConcurrentSet};
+use std::{thread, time::Instant};
+
+use rand::{rngs::SmallRng, Rng, SeedableRng};
+
+#[derive(Clone, Copy, Default, Debug)]
+struct BenchmarkResult {
+    reads: usize,
+    read_times: u128,
+    insert: usize,
+    insert_times: u128,
+    delete: usize,
+    delete_times: u128,
+    ops: usize,
+    op_times: u128,
+}
+
+#[derive(Clone, Copy)]
+struct BenchmarkConfig {
+    write_ratio: f64,
+    insert_ratio: f64,
+    n_threads: u8,
+    timeout: u128,
+    initial_size: usize,
+    range: usize,
+}
+
+fn read_write(set: ConcurrentBTreeSet<usize>, config: BenchmarkConfig) -> BenchmarkResult {
+    let worker = || {
+        let set = set.clone_ref();
+
+        thread::spawn(move || {
+            let mut results: BenchmarkResult = BenchmarkResult::default();
+            let mut _rnd = SmallRng::from_seed([0; 16]);
+            let start = Instant::now();
+            // initialize thread
+            loop {
+                if start.elapsed().as_millis() > config.timeout {
+                    break;
+                }
+
+                let i = Instant::now();
+                let num = _rnd.gen_range(0, config.range);
+                if _rnd.gen::<f64>() < config.write_ratio {
+                    //println!("write op: {}, thread {}", ops, n_threads);
+                    if _rnd.gen::<f64>() < config.insert_ratio {
+                        let curr = Instant::now();
+                        set.delete(num);
+                        results.delete += 1;
+                        results.delete_times += curr.elapsed().as_nanos();
+                    } else {
+                        let curr = Instant::now();
+                        set.insert(num);
+                        results.insert += 1;
+                        results.insert_times += curr.elapsed().as_nanos();
+                    }
+                } else {
+                    let curr = Instant::now();
+                    // read operation
+                    set.contains(num);
+                    results.reads += 1;
+                    results.read_times += curr.elapsed().as_nanos();
+                }
+
+                results.ops += 1;
+                results.op_times += i.elapsed().as_nanos();
+            }
+
+            //println!("Results for {} threads: {:?}", config.n_threads, results);
+            results
+        })
+    };
+
+    let threads: Vec<_> = (0..config.n_threads).map(|_| worker()).collect();
+    threads
+        .into_iter()
+        .map(|t| t.join().unwrap())
+        .fold(BenchmarkResult::default(), |mut x, res| {
+            x.ops += res.ops;
+            x.op_times += res.op_times;
+
+            x.reads += res.reads;
+            x.read_times += res.read_times;
+
+            x.insert += res.insert;
+            x.insert_times += res.insert_times;
+
+            x.delete += res.delete;
+            x.delete_times += res.delete_times;
+
+            x
+        })
+}
+
+fn benchmark() {
+    println!("Write_Fraction,Thread_Count,Throughput");
+    for wr in &[0.02, 0.2, 0.4] {
+        for i in 1..=8 {
+            let config = BenchmarkConfig {
+                write_ratio: *wr,
+                insert_ratio: 0.5,
+                n_threads: i,
+                timeout: 10000,
+                initial_size: 256,
+                range: 512,
+            };
+
+            let ops: Vec<_> = (0..3)
+                .map(|_| {
+                    let set = ConcurrentBTreeSet::new();
+                    let mut _rnd = SmallRng::from_seed([0; 16]);
+                    while set.len() < config.initial_size {
+                        let i = _rnd.gen_range(0, config.range);
+                        set.insert(i);
+                    }
+                    read_write(set, config)
+                })
+                .collect();
+
+            let avg: f64 =
+                (ops.iter().map(|res| res.ops).sum::<usize>() as f64) / (ops.len() as f64);
+            let throughput = avg / ((config.timeout * 1000) as f64);
+
+            println!("{},{},{}", wr, i, throughput);
+        }
+    }
+}
+
+fn main() {
+    benchmark();
+}
